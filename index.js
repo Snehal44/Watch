b4f0921fccc6821f2557919d14c773e601c5c607
Snehal44 <77604064+Snehal44@users.noreply.github.com>
2021-02-17 11:33:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+<script>
+  window.console = window.console || function(t) {};
+</script>
+
+  
+  
+  <script>
+  if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+  }
+</script>
